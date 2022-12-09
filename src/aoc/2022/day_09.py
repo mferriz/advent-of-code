125dded50fe6276e4f5eb4a77cb8c3433fdab366
Mario A. Ferriz <mario_ferriz@trendmicro.com>
2022-12-09 15:54:43 -0600

    Adding solution for AoC 2022, day 9

--- src/aoc/2022/day_09.py
+++ src/aoc/2022/day_09.py
@@ -7,42 +7,54 @@ import numpy
 
 INPUT_FILE = 'data/day_09.txt'
 OPERATION = {
-    'R': (1, 0),
-    'U': (0, 1),
-    'L': (-1, 0),
-    'D': (0, -1)
+    'R': numpy.array((1, 0)),
+    'U': numpy.array((0, 1)),
+    'L': numpy.array((-1, 0)),
+    'D': numpy.array((0, -1)),
 }
 FOLLOW = {
-    (2, 0): (1, 0),
-    (-2, 0): (-1, 0),
-    (0, 2): (0, 1),
-    (0, -2): (0, -1),
-    (1, 2): (1, 1),
-    (1, -2): (1, -1),
-    (-1, 2): (-1, 1),
-    (-1, -2): (-1, -1),
-    (2, 1): (1, 1),
-    (2, -1): (1, -1),
-    (-2, 1): (-1, 1),
-    (-2, -1): (-1, -1)
+    (2, 0): numpy.array((1, 0)),
+    (-2, 0): numpy.array((-1, 0)),
+    (0, 2): numpy.array((0, 1)),
+    (0, -2): numpy.array((0, -1)),
+    (1, 2): numpy.array((1, 1)),
+    (1, -2): numpy.array((1, -1)),
+    (-1, 2): numpy.array((-1, 1)),
+    (-1, -2): numpy.array((-1, -1)),
+    (2, 1): numpy.array((1, 1)),
+    (2, -1): numpy.array((1, -1)),
+    (-2, 1): numpy.array((-1, 1)),
+    (-2, -1): numpy.array((-1, -1)),
+    (2, 2): numpy.array((1, 1)),
+    (2, -2): numpy.array((1, -1)),
+    (-2, 2): numpy.array((-1, 1)),
+    (-2, -2): numpy.array((-1, -1)),
 }
 
 
-def main() -> None:
+def simulate(knots: int = 2) -> int:
     """Simulate rope movement."""
     with open(INPUT_FILE, encoding='utf-8') as input_file:
         tail_locations = set()
-        head = (0, 0)
-        tail = (0, 0)
-        tail_locations.add(tail)
+        rope = [numpy.array((0, 0)) for _ in range(knots)]
+        tail_locations.add(tuple(rope[-1]))
         for line in input_file:
             instruction, quantity = line.strip().split(' ')
             for _ in range(int(quantity)):
-                head = tuple(numpy.add(head, OPERATION[instruction]))
-                if (delta := tuple(numpy.subtract(head, tail))) in FOLLOW:
-                    tail = tuple(numpy.add(tail, FOLLOW[delta]))
-                    tail_locations.add(tail)
-        print(f'Part One: Positions tail of rope visit: {len(tail_locations)}')
+                rope[0] += OPERATION[instruction]
+                for knot in range(1, len(rope)):
+                    if (delta := tuple(rope[knot - 1] - rope[knot])) in FOLLOW:
+                        rope[knot] += FOLLOW[delta]
+                    else:
+                        break
+                tail_locations.add(tuple(rope[-1]))
+    return len(tail_locations)
+
+
+def main() -> None:
+    """Simulate a couple of rope movements."""
+    print(f'Part One: Positions tail of visits (2 knots): {simulate(2)}')
+    print(f'Part One: Positions tail of visits (10 knots): {simulate(10)}')
 
 
 main()
